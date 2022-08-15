fb05b6d424783829c432532ff698e9edb399a6ea
djlabat <55716431+djlabat@users.noreply.github.com>
2022-08-15 15:36:25 +0200

    Sass import → forward-use

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// JSON data - Source
+// JSON data for chart
 import data from './data.json' assert {type: 'json'}; // This line works only in Chrome
 
-// HTML Colection of days - Target
+// HTML Colection of days in chart - Target
 let chart = document.querySelector(".chart").children
 
-// Source→ Target
+// JSON data → Target
 for (let i=0; i<7; i++) {
     chart[i].style.marginTop = 149 - data[i].amount + "px"
     chart[i].innerText = data[i].amount
 }
 
+// Cyan color dotay's bar
 let today = new Date()
-
-//   [  today's bar↓       ]
-chart[(today.getDate()-1)%6].style.backgroundColor = "hsl(186, 34%, 60%)"
-chart[(today.getDate()-1)%6].classList.add("today")
-
+chart[(today.getDay()+6)%7].classList.add("today")
